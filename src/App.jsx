85c1d61d4e2ecaf024f8edd387ad0d3a8f038808
zamuwelle <xopxtech@gmail.com>
2026-07-27 12:22:36 +0800

    The summoning is complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function ColorBackground() {
+	return <div className="fixed inset-0 animated-bg" />
+}
+
+function App() {
+	return (
+		<>
+			<ColorBackground />
+		</>
+	)
+}
+
+export default App
