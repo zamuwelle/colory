af41454ae2f5683e64ac94e1713c9d2ba22c4948
zamuwelle <xopxtech@gmail.com>
2026-07-28 19:07:13 +0800

    Remove React boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import './index.css'
-
-function ColorBackground() {
-	return <div className="fixed inset-0 animated-bg" />
-}
-
 function App() {
 	return (
 		<>
-			<ColorBackground />
 		</>
 	)
 }
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 	base: '/colory/',
-  plugins: [
+	plugins: [
 		react(),
 		tailwindcss()
 	],
