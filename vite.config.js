b14319605bebd146f313beb8da9cf45527be61d4
zamuwelle <xopxtech@gmail.com>
2026-07-28 19:03:58 +0800

    Change URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/color-cycle-react/',
+	base: '/colory/',
   plugins: [
 		react(),
 		tailwindcss()
